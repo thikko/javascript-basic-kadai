316bb207ac514d10f37238f878c28656622e1278
thikko <aosaku706@icloud.com>
2024-07-24 02:19:29 +0900

    ５章課題修正
    5章課題修正しました。

--- kadai__005/bmi.js
+++ kadai__005/bmi.js
@@ -3,10 +3,14 @@ let bodyWeight;
 let bodyHeight;
 let aFormula;
 
-//変数の代入
-bodyWeight = '68kg'
-bodyHeight = '1.7m'
-aFormula = [68] / [1.7 * 1.7]
+// //変数の代入
+// bodyWeight = '68kg'
+// bodyHeight = '1.7m'
+// aFormula = [68] / [1.7 * 1.7]
+
+bodyWeight = 68;
+bodyHeight = 1.7;
+aFormula = (bodyWeight) / (bodyHeight* bodyHeight);
 
 //コンソールへの出力
 console.log(aFormula);
